58b245a1e13e42a3fc94d70076ca0206cbc3c0e2
OmerOzen <omerfaruko423@gmail.com>
2023-12-04 12:57:59 +0100

    Added Shop Page Product Detail Page Blog page and About Page and responsive

--- script.js
+++ script.js
@@ -2,15 +2,23 @@ const bar = document.getElementById('bar');
 const nav = document.getElementById('navbar');
 const closee = document.getElementById('close');
 
+const MainImg = document.getElementById("MainImg");
+const smallImages = document.querySelectorAll(".small-img");
+
 if (bar) {
     bar.addEventListener('click', () => {
         nav.classList.add('active');
     });
 }
 
-if (close) {
+if (closee) {
     closee.addEventListener('click', () => {
         nav.classList.remove('active');
     });
 }
 
+smallImages.forEach(function(smallImage) {
+    smallImage.addEventListener("click", function() {
+        MainImg.src = smallImage.src;
+    });
+});
